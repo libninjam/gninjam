1269bf95018e1141627bc4715fb9e6be5c0bdcd2
tobias <tobias@90ee38a3-2122-0410-8c3a-b6451363bf28>
2006-11-16 03:33:45 +0000

    added remote channel strips

---
+++ src/common.hh
@@ -0,0 +1,7 @@
+#ifndef _COMMON_HH
+#  define _COMMON_HH
+#include <glibmm/ustring.h>
+
+Glib::ustring on_hscale_volume_format_value(double value);
+Glib::ustring on_hscale_pan_format_value(double value);
+#endif
