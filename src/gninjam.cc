0d7142249163e1a781b5303d3a6edba129089711
tobias <tobias@90ee38a3-2122-0410-8c3a-b6451363bf28>
2006-11-23 17:54:37 +0000

    fix missing POTFILES.in and duplicate definition of _

--- src/gninjam.cc
+++ src/gninjam.cc
@@ -17,27 +17,6 @@
 */
 
 #include <config.h>
-/*
- * Standard gettext macros.
- */
-#ifdef ENABLE_NLS
-#  include <libintl.h>
-#  undef _
-#  define _(String) dgettext (GETTEXT_PACKAGE, String)
-#  ifdef gettext_noop
-#    define N_(String) gettext_noop (String)
-#  else
-#    define N_(String) (String)
-#  endif
-#else
-#  define textdomain(String) (String)
-#  define gettext(String) (String)
-#  define dgettext(Domain,Message) (Message)
-#  define dcgettext(Domain,Message,Type) (Message)
-#  define bindtextdomain(Domain,Directory) (Domain)
-#  define _(String) (String)
-#  define N_(String) (String)
-#endif
 
 #include <gtkmm/main.h>
 #include <glib/gi18n.h>
