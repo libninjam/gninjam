966529948a00684c6e8a3146afeac73880a90cdb
tobias <tobias@90ee38a3-2122-0410-8c3a-b6451363bf28>
2006-11-23 18:28:51 +0000

    fix markup problem

--- trunk/src/gNinjamClient.cc
+++ trunk/src/gNinjamClient.cc
@@ -363,7 +363,7 @@ void gNinjamClient::addChatText(Glib::ustring text)
 
 void gNinjamClient::setChatTopic(Glib::ustring text)
 {
-  label_chat->set_text(_("<b>Chat</b>: ")+text);
+  label_chat->set_text(text);
 }
 
 void gNinjamClient::update_inputLists()
