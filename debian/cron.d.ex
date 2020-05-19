b47f5eebb1bff6eeb6ff63a7ded67e67afebb780
bill-auger <mr.j.spam.me@gmail.com>
2020-05-19 10:17:16 -0400

    update debian packaging from kxstudio

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gninjam package
-#
-0 4	* * *	root	gninjam_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gninjam):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="gninjam" command="/usr/bin/gninjam"
